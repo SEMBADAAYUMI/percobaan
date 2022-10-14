e2b4bca10bb335f31a7194a04986d41d339bcb3b
SEMBADAAYUMI <sembadaayumiasyrafaufa@gmail.com>
2022-10-15 01:21:08 +0700

    menambahkan fitur baru

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
+  btn.addEventListener("click", (e) => {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
       count--;
